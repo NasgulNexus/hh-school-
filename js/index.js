251f2ebae678dcf1775f325fb0d8d5f19d61c931
NasgulNexus <moctegor@gmail.com>
2020-02-11 22:24:16 +0300

    Домашка

---
+++ js/index.js
@@ -0,0 +1,9 @@
+import './grid';
+import './slider';
+import './cities';
+import './popup';
+import './order-form/order-form';
+import './validations/validation'
+import './validations/areas';
+import './validations/forSubmit';
+import '../css/styles.less';
